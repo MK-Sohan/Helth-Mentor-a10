a1d207f67dd6f844fe10c58e4b1afdbbc943e691
MK-Sohan <mksohan786@gmail.com>
2022-04-17 16:41:55 +0600

    build checkout

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,9 +1,31 @@
 import React from "react";
+import "./Checkout.css";
 
 const Checkout = () => {
   return (
-    <div>
-      <h1>this is checkout </h1>
+    <div className="checkout-container">
+      <div className="all-container">
+        <h1 className="checkout-title">
+          Make An <br /> Appointment{" "}
+        </h1>
+        <div className="checkout-inputs">
+          <div className="input-1">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+          </div>
+
+          <div className="input-2">
+            <input
+              type="email"
+              name="checkout-email"
+              id=""
+              placeholder="Your Email"
+            />{" "}
+          </div>
+          <br />
+          <button className="submit-button">Submit Now</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,19 +1,30 @@
 import React from "react";
 import Serviceshooks from "../../../Hooks/Serviceshooks";
 import Service from "../Service/Service";
+import image from "../../../banner/Heart-beat.png";
 import "./Services.css";
 
 const Services = () => {
   const [services, setServices] = Serviceshooks();
   return (
     <div>
+      <div className="why-choosing-us">
+        <h1 className="text-center mt-5">Why Choosing Health Coach</h1> <br />
+        <img src={image} alt="" />
+        <p className="text-center ">
+          We handpick the best coaches and health experts from across the
+          country to make sure you get the most personalized health care you{" "}
+          <br />
+          deserve between those doctor visits.
+        </p>
+      </div>
       <div className="service-header">
         <h1 className="service-title">My All Services</h1>
         <p className="service-describe">
           Now a days lots of peoples are suffering different kind of mental
           diseases. But they don't know how to cout it off from their life.{" "}
-          <br /> As a personal helth trainer I will help you how to come out
-          from this type of mental issuses.
+          <br /> As a personal helth Coach I will help you how to come out from
+          this type of mental issuses.
         </p>
       </div>
       <div className="main">
